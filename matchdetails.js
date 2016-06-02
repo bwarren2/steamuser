9c9102e4bfc437a187bda58f74eef2b8d3ec6711
Ben Warren <bwarren2@gmail.com>
2016-06-02 16:14:50 -0700

    Bots can get match details

--- matchdetails.js
+++ matchdetails.js
@@ -1,8 +1,8 @@
 var SteamUser = require('steam-user');
 var generatePassword = require("password-generator");
+var dota2 = require("dota2");
 
 var steamuser = new SteamUser();
-
 
 var cleanup = require('./helpers/cleanup.js').Cleanup(myCleanup)
 
@@ -21,22 +21,26 @@ steamuser.on('error', function(e) {
     console.log(e);
 });
 
-var password = generatePassword();
-var account_name = generatePassword();
-var email = "dotashidduch+" + account_name + "@gmail.com";
-
-
 steamuser.on('loggedOn', function(details) {
     console.log("Logged into Steam as " + steamuser.steamID.getSteam3RenderedID());
     steamuser.setPersona(SteamUser.EPersonaState.Online);
     steamuser.gamesPlayed(570);
 
-    steamuser.createAccount(
-        account_name, password, email, function(result, steamid){
-        if (steamid){
-            console.log("created account : " + account_name + ":" + password + " [steamID]" + steamid);
-        }
-        console.log(result);
+    var Dota2 = new dota2.Dota2Client(steamuser.client, true);
+    Dota2.launch();
+    Dota2.on("ready", function () {
+        console.log("Dota 2 ready");
+        ready = true;
+        Dota2.requestMatchDetails(2408280426, function (err, body) {
+          // if (!self.match_deferreds[matchId]) {
+          //   return;
+          // }
+          console.log(body);
+          // self.match_deferreds[matchId].resolve(body);
+        });
     });
+
 });
 console.log('EOF');
+
+
